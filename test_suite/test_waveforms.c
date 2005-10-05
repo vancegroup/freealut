237ce01208fcf3521a56adf3b9963471210b80f3
svenp <svenp@109fce75-3012-0410-ac6a-932efec96c1b>
2005-10-05 12:13:34 +0000

    Ooops, forgot one file in the last commit.
    git-svn-id: svn://connect.creativelabs.com/trunk@695 109fce75-3012-0410-ac6a-932efec96c1b

--- test_suite/test_waveforms.c
+++ test_suite/test_waveforms.c
@@ -2,59 +2,30 @@
 #include <AL/alut.h>
 
 /*
-  This program plays a 440Hz tone using a variety of waveforms.
-*/
+ * This program plays a 440Hz tone using a variety of waveforms.
+ */
+
+static void
+playTone (ALenum waveshape)
+{
+  ALuint buffer, source;
+  buffer = alutCreateBufferWaveform (waveshape, 440, 0, 1);
+  alGenSources (1, &source);
+  alSourcei (source, AL_BUFFER, buffer);
+  alSourcePlay (source);
+  alutMicroSleep (1000000);
+}
 
 int
 main (int argc, char **argv)
 {
-  ALuint beep1Buffer, beep1Source;
-  ALuint beep2Buffer, beep2Source;
-  ALuint beep3Buffer, beep3Source;
-  ALuint beep4Buffer, beep4Source;
-  ALuint beep5Buffer, beep5Source;
-  int i;
-
   alutInit (&argc, argv);
 
-  beep1Buffer = alutCreateBufferWaveform (ALUT_WAVEFORM_SINE,
-                                          440.0f, 0.0f, 1.0f);
-  alGenSources (1, &beep1Source);
-  alSourcei (beep1Source, AL_BUFFER, beep1Buffer);
-
-  beep2Buffer = alutCreateBufferWaveform (ALUT_WAVEFORM_SQUARE,
-                                          440.0f, 0.0f, 1.0f);
-  alGenSources (1, &beep2Source);
-  alSourcei (beep2Source, AL_BUFFER, beep2Buffer);
-
-  beep3Buffer = alutCreateBufferWaveform (ALUT_WAVEFORM_SAWTOOTH,
-                                          440.0f, 0.0f, 1.0f);
-  alGenSources (1, &beep3Source);
-  alSourcei (beep3Source, AL_BUFFER, beep3Buffer);
-
-  beep4Buffer = alutCreateBufferWaveform (ALUT_WAVEFORM_WHITENOISE,
-                                          440.0f, 0.0f, 1.0f);
-  alGenSources (1, &beep4Source);
-  alSourcei (beep4Source, AL_BUFFER, beep4Buffer);
-
-  beep5Buffer = alutCreateBufferWaveform (ALUT_WAVEFORM_IMPULSE,
-                                          440.0f, 0.0f, 1.0f);
-  alGenSources (1, &beep5Source);
-  alSourcei (beep5Source, AL_BUFFER, beep5Buffer);
-
-  for (i = 0; i < 3; i++)
-    {
-      alSourcePlay (beep1Source);
-      alutMicroSleep (1000000);
-      alSourcePlay (beep2Source);
-      alutMicroSleep (1000000);
-      alSourcePlay (beep3Source);
-      alutMicroSleep (1000000);
-      alSourcePlay (beep4Source);
-      alutMicroSleep (1000000);
-      alSourcePlay (beep5Source);
-      alutMicroSleep (1000000);
-    }
+  playTone (ALUT_WAVEFORM_SINE);
+  playTone (ALUT_WAVEFORM_SQUARE);
+  playTone (ALUT_WAVEFORM_SAWTOOTH);
+  playTone (ALUT_WAVEFORM_WHITENOISE);
+  playTone (ALUT_WAVEFORM_IMPULSE);
 
   alutExit ();
   return EXIT_SUCCESS;
