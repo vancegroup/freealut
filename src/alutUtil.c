50bfe39b1e1e9986b9e79721ea302d01ea42cc45
svenp <svenp@109fce75-3012-0410-ac6a-932efec96c1b>
2006-02-07 14:43:34 +0000

    When nanosleep returns early due to a signal, continue sleeping, otherwise return AL_FALSE from alutSleep.
    git-svn-id: svn://connect.creativelabs.com/trunk@1007 109fce75-3012-0410-ac6a-932efec96c1b

--- src/alutUtil.c
+++ src/alutUtil.c
@@ -2,6 +2,7 @@
 
 #if HAVE_NANOSLEEP && HAVE_TIME_H
 #include <time.h>
+#include <errno.h>
 #elif HAVE_USLEEP && HAVE_UNISTD_H
 #include <unistd.h>
 #elif HAVE_SLEEP && HAVE_WINDOWS_H
@@ -22,27 +23,46 @@ alutSleep (ALfloat duration)
   {
     ALuint seconds = (ALuint) duration;
     ALfloat rest = duration - (ALfloat) seconds;
+
 #if HAVE_NANOSLEEP && HAVE_TIME_H
+
     ALuint microSecs = (ALuint) (rest * 1000000);
-    struct timespec t;
+    struct timespec t, remainingTime;
     t.tv_sec = (time_t) seconds;
     t.tv_nsec = ((long) microSecs) * 1000;
-    nanosleep (&t, NULL);
+
+    /* At least the interaction of nanosleep and signals is specified! */
+    while (nanosleep (&t, &remainingTime) < 0)
+      {
+        if (errno != EINTR)
+          {
+            return AL_FALSE;
+          }
+        /* If we received a signal, let's try again with the remaining time. */
+        t.tv_sec = remainingTime.tv_sec;
+        t.tv_nsec = remainingTime.tv_nsec;
+      }
+
 #elif HAVE_USLEEP && HAVE_UNISTD_H
+
     while (seconds > 0)
       {
         usleep (1000000);
         seconds--;
       }
     usleep ((unsigned int) (rest * 1000000));
+
 #elif HAVE_SLEEP && HAVE_WINDOWS_H
+
     while (seconds > 0)
       {
         Sleep (1000);
         seconds--;
       }
     Sleep ((DWORD) (rest * 1000));
+
 #endif
+
   }
   return AL_TRUE;
 }
